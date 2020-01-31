57b4e166f17097d57c00b89b6d19b6285008644c
Jacob Moss <jmoss@indeed.com>
2020-01-30 22:54:35 -0600

    stuck on linter problem

---
+++ linter.py
@@ -0,0 +1,12 @@
+if x==1:
+    return 'x is 1'
+elif x > 1:
+    if x > 3:
+        return 'x is greater than three'
+    else:
+        return 'x is 2'
+
+def linter(my_string):
+    
+
+my_string = ''''''

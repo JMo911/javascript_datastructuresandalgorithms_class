e6b2a720184afd67f704ba35f7809223cd0b297e
Jacob Moss <jmoss@indeed.com>
2020-02-11 00:06:35 -0600

    getting started with graphs

---
+++ graphs.js
@@ -0,0 +1,15 @@
+//trees -> special type of a graph
+//implement a graph using an adjacency list
+//traverse through a graph using BFS and DFS
+    //A GRAPH IS A COLLECTION OF NODES AND CONNECTIONS BETWEEN THOSE NODES
+    //NODES AND CONNECTIONS
+    //
+
+
+//USES - VERY COOL
+    //SOCIAL NETWORKS - 
+    //GOOGLE MAPS
+    //RECOMMENDATIONS (SPOTIFY, NETFLIX, ETC.)
+
+//vertex (node)
+//edge (connection between two nodes)
